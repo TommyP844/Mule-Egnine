72d6e5dbe39e36720c089eb5e292c9547f3c5a1d
Thomas Payne <tommypayne093@gmail.com>
2025-03-19 20:00:51 -0400

    Merged MF-1

--- MuleScriptEngine/obj/Debug/MuleScriptEngine.AssemblyInfo.cs
+++ MuleScriptEngine/obj/Debug/MuleScriptEngine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuleScriptEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60e5c813e9c737fea71ece3f025d26bb7208a3a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f39104f8bc37c5f399232e5d379d4f6eb0faabc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuleScriptEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuleScriptEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
